4d9bf670d4096de37c55d76f3821282425074ee3
Heba <hebamirdora@gmail.com>
2022-04-02 15:05:48 +0200

    map a list of expenses in the App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import ExpenseItem from "./ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id : 0,
+      title: 'Car Insurance',
+      Date : new Date(2022, 4, 2),
+      price: 193,
+    },
+    {
+      id : 0,
+      title: 'Car Insurance',
+      Date : new Date(2022, 4, 2),
+      price: 193,
+    },
+    {
+      id : 0,
+      title: 'Car Insurance',
+      Date : new Date(2022, 4, 2),
+      price: 193,
+    }
+  ]
   return (
     <div className="App">
-      <ExpenseItem/>
+     {
+       expenses.map((item)=>
+        <ExpenseItem
+        title={item.title}
+        price={item.price}
+        data={item.price}
+        />
+       
+       
+       )
+     } 
    
     </div>
   );
--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 4, 2)
-    const expenseItem = 'Car Insurance'
-    const expensePrice = 269
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date}</div>
         <div className='expense-item__description'>
-            <h2>{expenseItem}</h2>
-            <div className='expense-item__price'>${expensePrice}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.price}</div>
         </div>
     </div>
     )
